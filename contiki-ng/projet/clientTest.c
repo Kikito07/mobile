5270ab988d8ee63a66bae400e6bb761609138846
Kikito <nikita.tyunyayev@gmail.com>
2021-04-29 16:23:43 +0200

    adding clients

---
+++ contiki-ng/projet/clientTest.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+  
+#define PORT 3000
+#define MAXLINE 1024
+
+#define SERVERADDRESS "bbbb::c30c:0:0:5"
+#define NODE1ADDR "bbbb::c30c:0:0:1"
+#define NODE2ADDR "bbbb::c30c:0:0:2"
+#define NODE3ADDR "bbbb::c30c:0:0:3"
+#define NODE4ADDR "bbbb::c30c:0:0:4"
+
+  
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char* data = "hello";
+    int number = 1256;
+    number = htonl(number);
+    struct sockaddr_in6 servaddr;
+  
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+  
+    memset(&servaddr, 0, sizeof(servaddr));
+      
+    // Filling server information
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_port = htons(PORT);
+    int err;
+    err = inet_pton(AF_INET6,NODE1ADDR,&servaddr.sin6_addr);
+    if(err <= 0){
+        printf("error");
+    }
+    int n, len;
+
+    err = sendto(sockfd, data, sizeof(data),
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+            sizeof(servaddr));
+
+    if(err < 0){
+        perror("Error printed by perror");
+    }
+
+    printf("Hello message sent.\n");
+          
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+                MSG_WAITALL, (struct sockaddr *) &servaddr,
+                &len);
+    buffer[n] = '\0';
+    printf("Server : %s\n", buffer);
+  
+    close(sockfd);
+    return 0;
+}
