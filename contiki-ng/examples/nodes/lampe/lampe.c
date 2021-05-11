79e1fd6fa597203d36737f2f59de933a21723a50
Kikito <nikita.tyunyayev@gmail.com>
2021-05-11 12:51:03 +0200

    fixito lamp

--- contiki-ng/examples/nodes/lampe/lampe.c
+++ contiki-ng/examples/nodes/lampe/lampe.c
@@ -47,46 +47,30 @@ static char buf[MAX_PAYLOAD_LEN];
 static char buf_hello[MAX_PAYLOAD_LEN];
 static uint16_t len;
 static struct etimer timer;
-static int helloDeviceDone = 0;
 static uip_ipaddr_t ipaddr;
 static size_t pkt_size = 5;
-static uint8_t token = 0;
-static pkt_t hello_pkt;
+
+static pkt_t pkt;
+
+static device_t device = LAMP;
+static uint8_t msgid = 0;
+static pcode_t hello = PCODE_HELLO;
 
 /*---------------------------------------------------------------------------*/
 
 static void 
 handle_packet()
 {   
-    pkt_t pkt;
+    
     
     if (PKT_OK != pkt_decode(buf, &pkt))
     {
         PRINTF("packet received but encode fail");
     }
-    PRINTF("code : %u\n",(uint8_t)pkt_get_code(&pkt));
-    PRINTF("hello msg : %u\n",PCODE_HELLO );
-    if(pkt_get_code(&pkt) == PCODE_HELLO){
-        PRINTF("je suis rentre dans le premier if\n");
-        if(helloDeviceDone == 0){
-            PRINTF("inside token\n");
-            token = *(pkt_get_payload(&pkt));
-            PRINTF("token lamp : %u\n",token);
-            pkt_set_token(&hello_pkt,token);
-            pkt_encode(&hello_pkt,buf_hello);
-            PRINTF("token apres encode : %u \n",pkt_get_token(&hello_pkt));
-
-            helloDeviceDone = 1;
-
-
-        }
-    }
-
     if (pkt_get_code(&pkt) == PCODE_POST)
     {
 
         const char *payload = pkt_get_payload(&pkt);
-        PRINTF("token : %u\n",pkt_get_token(&pkt));
         PRINTF("msgid : %u\n",pkt_get_msgid(&pkt));
         lamp_types_t post_type = payload[0];
         printf("post_type : %u\n",post_type);
@@ -95,26 +79,16 @@ handle_packet()
             leds_on(LEDS_RED);
             uint8_t one = 1;
             pkt_set_ack(&pkt,one);
-            PRINTF("ddddddddddddd");
-            PRINTF("ack : %u\n",pkt_get_ack(&pkt));
-            PRINTF("mais putain j'envois le ack mes couilles\n");
+            pkt_set_device(&pkt,device);
         }
         if (post_type == PTYPE_LIGHT_OFF)
         {
-            PRINTF("hello off\n");
             leds_off(LEDS_RED);
         }
     }
     pkt_set_ack(&pkt, 1);
     pkt_encode(&pkt, buf);
-    uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
-    PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
-    PRINTF(":%u\n", UIP_HTONS(UIP_UDP_BUF->srcport));
-
-    server_conn->rport = UIP_UDP_BUF->srcport;
     uip_udp_packet_send(server_conn, buf, len);
-    uip_create_unspecified(&server_conn->ripaddr);
-    server_conn->rport = 0;
 }
 
 static void
@@ -130,13 +104,6 @@ tcpip_handler(void)
         PRINTF("]:%u\n", UIP_HTONS(UIP_UDP_BUF->srcport));
         handle_packet();
         printf("packet : %u \n", *buf);
-    #if SERVER_REPLY
-        uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
-        server_conn->rport = UIP_UDP_BUF->srcport;
-        uip_udp_packet_send(server_conn, buf, len);
-        uip_create_unspecified(&server_conn->ripaddr);
-        server_conn->rport = 0;
-    #endif
     }
     return;
 }
@@ -180,39 +147,28 @@ PROCESS_THREAD(udp_server_process, ev, data)
     uip_ip6addr(&ipaddr,0xBBBB,0,0,0,0,0,0,0x1);
     server_conn = udp_new(&ipaddr, UIP_HTONS(3000), NULL);
     udp_bind(server_conn, UIP_HTONS(3000));
-    uint8_t msgid = 5;
-    pcode_t hell = PCODE_HELLO;
+    static pkt_t hello_pkt;
     pkt_set_msgid(&hello_pkt,msgid);
-    pkt_set_token(&hello_pkt,token);
-    pkt_set_code(&hello_pkt, hell);
+    pkt_set_code(&hello_pkt, hello);
+    pkt_set_device(&hello_pkt,device);
+    pkt_set_ack(&hello_pkt, 0);
     pkt_encode(&hello_pkt, buf_hello);
-
+    
     PRINTF("Listen port: 3000, TTL=%u\n", server_conn->ttl);
 
-    etimer_set(&timer, 1 * CLOCK_CONF_SECOND);
+    etimer_set(&timer, 3 * CLOCK_CONF_SECOND);
     while (1)
     { 
         PROCESS_YIELD();
         if (ev == PROCESS_EVENT_TIMER)
-        {
-            // PRINTF("normal pritn\n");
-            
-            if(helloDeviceDone == 0){
-                uip_udp_packet_send(server_conn, buf_hello, pkt_size);
-                // PRINTF("HELLO\n");
-
-            }
-            else{
-                uip_udp_packet_send(server_conn, buf_hello, pkt_size);
-            }
+        {  
+            uip_udp_packet_send(server_conn, buf_hello, pkt_size);
             etimer_reset(&timer);
             
         }
         else if(ev == tcpip_event){
-            PRINTF("hello tcp\n");
+            PRINTF("tcp ip event\n");
             tcpip_handler();
-            etimer_set(&timer, 3 * CLOCK_CONF_SECOND);
-
         }
     }
     PROCESS_END();
